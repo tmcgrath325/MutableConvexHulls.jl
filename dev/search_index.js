aa5704855c9cb0557d7780fd97702f0d22ce7128
Documenter.jl <documenter@juliadocs.github.io>
2022-07-21 16:13:59 +0000

    build based on d2fcc6b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MutableConvexHulls","category":"page"},{"location":"#MutableConvexHulls","page":"Home","title":"MutableConvexHulls","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MutableConvexHulls.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MutableConvexHulls]","category":"page"}]
+}
